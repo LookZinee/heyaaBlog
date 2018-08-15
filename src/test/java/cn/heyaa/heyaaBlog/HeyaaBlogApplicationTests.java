3ee7ee6bd080af7dc363cda91bb06cd4bef44030
刘子义 <ziyiliu@huashenghaoche.com>
2018-08-15 21:17:45 +0800

    heyaa project

---
+++ src/test/java/cn/heyaa/heyaaBlog/HeyaaBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.heyaa.heyaaBlog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeyaaBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
