771c71f0ec885acad0817ac03fa00ea3e52cbdc3
刘子义 <ziyiliu@huashenghaoche.com>
2018-08-16 09:33:17 +0800

    添加首页

--- src/main/java/cn/heyaa/heyaaBlog/HeyaaBlogApplication.java
+++ src/main/java/cn/heyaa/heyaaBlog/HeyaaBlogApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @SpringBootApplication
@@ -12,6 +15,12 @@ public class HeyaaBlogApplication {
 	@RequestMapping("/")
 	String index(){
 		return "Hello Spring Boot";
+	}
+
+	@RequestMapping("/toIndex")
+	ModelAndView toIndex(HttpServletRequest req, ModelAndView view){
+		view.setViewName("web/index.html");
+		return view;
 	}
 
 	public static void main(String[] args) {
