fc1f6bef3898477aa4ee59bf91bee83bdeced707
刘子义 <ziyiliu@huashenghaoche.com>
2018-08-17 09:49:07 +0800

    绑定域名

--- src/main/java/cn/heyaa/heyaaBlog/controller/TestController.java
+++ src/main/java/cn/heyaa/heyaaBlog/controller/TestController.java
@@ -3,6 +3,9 @@ package cn.heyaa.heyaaBlog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: ziyi.liu
@@ -10,12 +13,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Version 1.0
  */
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
 
     @ResponseBody
-    @RequestMapping("/login")
+    @RequestMapping("/")
     public String login(){
-        return "hello world";
+        return "kansha gun a";
+    }
+
+    @RequestMapping("/tologin")
+    public ModelAndView login(HttpServletRequest req, ModelAndView view){
+        System.out.println("进入主页");
+        view.setViewName("/web/index.html");
+        return view;
     }
 }
